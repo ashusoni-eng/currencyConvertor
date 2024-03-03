8a08a68c4246ab6e6b7f24a396504204ba31ab23
ashusoni-eng <ashishsoni.mp07@gmail.com>
2024-03-03 18:11:23 +0530

    ALl structure and styles

---
+++ app.js
@@ -0,0 +1,16 @@
+const dropdowns= document.querySelectorAll(".currency");
+
+
+// for(let select of dropdowns){
+//     for(currCode in countryList){
+//         let option= document.createElement("option");
+//         option.value=code;
+//         option.innerText= code;
+//         select.appendChild(option);
+//     };
+// }
+
+// for(code in countryList){
+//     console.log(code);
+// }
+console.log(countryList);
